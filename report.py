2488310e815439368f2d08108f7bab5b473a59b2
geomaedhros <geomaedhros@hotmail.com>
2021-07-12 19:52:21 +0200

    archivo inicial creado

---
+++ report.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import json
+import locale
+import sys
+
+
+def load_data(filename):
+  """Loads the contents of filename as a JSON file."""
+  with open(filename) as json_file:
+    data = json.load(json_file)
+  return data
+
+
+def format_car(car):
+  """Given a car dictionary, returns a nicely formatted name."""
+  return "{} {} ({})".format(
+      car["car_make"], car["car_model"], car["car_year"])
